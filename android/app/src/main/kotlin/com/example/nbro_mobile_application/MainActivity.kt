2e286746439156fe68aee260241303a8f6eddc02
Gunath kalpitha <gbgunathkalpitha18@gmail.com>
2026-04-09 14:39:48 +0530

    fix fingerprint settings

--- android/app/src/main/kotlin/com/example/nbro_mobile_application/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/nbro_mobile_application/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.nbro_mobile_application
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
